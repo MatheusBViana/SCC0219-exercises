0c5ed8a56158956e99d0686daf7669957f8107a4
Matheus Bermudes Viana <matheusbviana@hotmail.com>
2025-03-11 22:00:23 -0300

    removing

--- ex10_vue/ex2/src/components/Signos.jsx
+++
@@ -1,7 +0,0 @@
-const Signos = [
-    "Signo", "Áries", "Leão", "Gêmeos", "Câncer", "Touro",
-    "Virgem", "Libra", "Escorpião", "Sagitário", "Capricórnio",
-    "Aquário", "Peixes"
-]
-
-export default Signos;
--- ex4_javascript/questao2.js
+++
@@ -1,9 +0,0 @@
-// Para verificar se um número é par ou ímpar, basta dividir
-// por 2, se o resto for 0 é impar
-for (let i = 0; i < 16; i++){
-    if(i % 2 == 0){
-        document.write(i +" is even<br>");
-    }else{
-        document.write(i +" is odd<br>");
-    }
-}
